c6c8903d1ac371dc9c7ea94d9a2f5ec2bddf5c18
J450N-ong <odeleyejason@gmail.com>
2024-09-23 19:46:28 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,17 @@
+a=10
+b=12
+c=0
+if a and b and c:
+    print("All the numbers are having boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+
+j=10
+k=-10
+l=0
+if j<0 or k>0:
+    print("However, j is greater than 0")
+elif k<0 or l!=0:
+    print("k is lesser than 0")
+else:
+    print("None is greater than 0")
